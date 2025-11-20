128bc89f97e208eac39c2d8cbb68c82a038994fd
eduardo.florencio <eduardo.florencio@linx.com.br>
2025-11-20 12:44:28 -0300

    feat: add dotenv support for environment variables and configure Firebase settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,16 @@
 // webpack.config.js
 const path = require('path');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
+
+// Call dotenv and it will return an Object with a parsed key
+const env = dotenv.config().parsed;
+
+// reduce it to a nice object, the same as before
+const envKeys = Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+  return prev;
+}, {});
 
 module.exports = {
   mode: 'development', // Use 'production' for minified output
@@ -20,5 +31,8 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public', 'dist'),
   },
+  plugins: [
+    new webpack.DefinePlugin(envKeys)
+  ],
   devtool: 'source-map', // Helps with debugging in the browser
 };
